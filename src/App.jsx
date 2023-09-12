e3e21c9fb9e56f006b1ba8b9d5c2f6822f4daf2c
Abusayed Shuvo <abusayedshuvo24@gmail.com>
2023-09-12 11:02:22 +0600

    start work

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-between">
+        <h1 className="text-4xl font-semibold">Knowledge Cafe</h1>
+        <img src="avatar-1.png" alt="" />
+      </div>
+    </>
+  );
+}
+
+export default App;
